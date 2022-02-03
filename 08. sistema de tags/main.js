784e0ae11eaf72493312f033e9e041b25c56b14f
Marcos Rivas <marcosrivasr@gmail.com>
2022-02-02 20:14:42 -0600

    input de etiquetas

---
+++ 08. sistema de tags/main.js
@@ -0,0 +1,67 @@
+let tags = [];
+const inputTagContainer = document.querySelector("#input-tag");
+const tagsContainer = document.createElement("div");
+const inputTag = document.createElement("span");
+
+inputTagContainer.addEventListener("click", (e) => {
+  console.log(e.target.id);
+  if (
+    e.target.id === "input-tag" ||
+    e.target.classList.contains("tag-container")
+  ) {
+    inputTag.focus();
+  }
+});
+
+inputTag.ariaRoleDescription = "textbox";
+inputTag.contentEditable = "true";
+inputTag.classList.add("input");
+inputTag.focus();
+
+inputTagContainer.classList.add("input-tag-container");
+tagsContainer.classList.add("tag-container");
+
+inputTagContainer.appendChild(tagsContainer);
+tagsContainer.appendChild(inputTag);
+
+inputTag.addEventListener("keypress", (e) => {
+  if (e.key === "Enter" && inputTag.textContent !== "") {
+    e.preventDefault();
+    if (!existTag(inputTag.textContent)) {
+      tags.push(inputTag.textContent);
+      inputTag.textContent = "";
+      renderTags();
+    }
+  }
+});
+
+function renderTags() {
+  tagsContainer.innerHTML = "";
+  const html = tags.map((tag) => {
+    const tagElement = document.createElement("div");
+    const tagButton = document.createElement("button");
+    tagElement.classList.add("tag-item");
+    tagButton.textContent = "X";
+    tagButton.addEventListener("click", (e) => {
+      removeTag(tag);
+    });
+    tagElement.appendChild(document.createTextNode(tag));
+    tagElement.appendChild(tagButton);
+    return tagElement;
+  });
+
+  html.forEach((element) => {
+    tagsContainer.appendChild(element);
+  });
+  tagsContainer.appendChild(inputTag);
+  inputTag.focus();
+}
+
+function existTag(value) {
+  return tags.includes(value);
+}
+
+function removeTag(value) {
+  tags = tags.filter((tag) => tag != value);
+  renderTags();
+}
